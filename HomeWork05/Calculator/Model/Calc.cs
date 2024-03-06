968e9bb105751e1e447f217c72f171818c0503a9
Aliaksandr Karpenka <kasasha@mail.ru>
2024-03-06 10:21:43 +0300

    HW05 Calculator

---
+++ HomeWork05/Calculator/Model/Calc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Model
+{
+    internal class Calc : ICalc
+    {
+        public event EventHandler<OperandChangedEventArgs> ChangeResult;
+        private Stack<decimal> stack = new Stack<decimal>();
+        private decimal Result
+        {
+            get
+            {
+                return stack.Count() == 0 ? 0 : stack.Peek();
+            }
+            set
+            {
+                stack.Push(value);
+                RaiseEvent();
+            }
+        }
+        public void RaiseEvent()
+        {
+            ChangeResult.Invoke(this, new OperandChangedEventArgs(Result));
+        }
+        public void CancelLast()
+        {
+            if (stack.Count > 0)
+            {
+                stack.Pop();
+                RaiseEvent();
+            }
+        }
+        public void Divide(decimal numder)
+        {
+            Result /= numder;
+        }
+
+        public void Multiply(decimal numder)
+        {
+            Result *= numder;
+        }
+
+        public void Substract(decimal numder)
+        {
+            Result -= numder;
+        }
+
+        public void Sum(decimal numder)
+        {
+            Result += numder;
+        }
+
+    }
+}
---
+++ HomeWork05/Calculator/Model/ICalc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Model
+{
+    internal interface ICalc
+    {
+        void Sum(decimal x);
+        void Substract(decimal x);
+        void Divide(decimal x);
+        void Multiply(decimal x);
+        void CancelLast();
+
+        event EventHandler<OperandChangedEventArgs> ChangeResult;
+
+    }
+}
