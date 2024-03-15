3e7d57a697967325f3fbf229d443ecb803430716
Aliaksandr Karpenka <kasasha@mail.ru>
2024-03-15 15:35:21 +0300

    HW08

---
+++ HomeWork08/FileFinder/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FileFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String path = args[0];
+            foreach (var item in Directory.GetFiles(path, "*."+args[1], SearchOption.AllDirectories)
+                .Where(item => new StreamReader(item).ReadToEnd().Contains(args[2])))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
